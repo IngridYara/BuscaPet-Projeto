d4ee6e6f7ab246ffbcdf0ed83787b71b48f5a576
IngridYara <Yaraalves3632659@gmail.com>
2024-06-30 02:34:38 -0300

    Vercel-BackEnd

--- docs/04 - video/README.md
+++
@@ -1,8 +0,0 @@
-# Vídeos do Projeto
-A relação abaixo lista os vídeos feitos para o projeto:
- - [Vídeo - Pitch](https://drive.google.com/file/d/1rw7Z5M4CyLtzElaiMC1h6NQRUEy9Je2v/view?usp=sharing)
- - [Vídeo BBBBBBBB]()
-
-> Nesta pasta inclua arquivos de vídeo produzidos para divulgação do 
-> projeto e seus resutados.
-
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./db/db.json')
+const router = jsonServer.router('./db.json')
 const cors = require('cors');
 
 const middlewares = jsonServer.defaults()
 
 server.use(cors())
 server.use(middlewares)
-
+server.use(
+  jsonServer.rewriter({
+    "/*": "/$1",
+  })
+)
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server está em execução em http://localhost:3000!')
